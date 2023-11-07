6335bafdb19ca92d1d7bd8db5e4792e7f5210d2c
Rowan Dash <rowan.dash@swansea.ac.uk>
2023-11-07 14:20:18 +0000

    feat: rbac rolebinding in helm chart (#20)

--- src/canary.py
+++ src/canary.py
@@ -1,9 +1,11 @@
 import asyncio
 import aiohttp
 
-import random
+# import random
 import logging
 import click
+from kubernetes_asyncio import client, config
+from kubernetes_asyncio.client.api_client import ApiClient
 
 
 logging.basicConfig(
@@ -54,23 +56,11 @@ async def monitor_url(name, url, interval, statuses):
 
 
 async def watch_events(*args, **kwargs):
+    config.load_incluster_config()
+
     logging.info("starting watcher")
     logging.debug(args)
     logging.debug(kwargs)
-    monitors = [
-        {
-            "name": "airflow",
-            "url": "https://airflow.sail-teleport.dk.serp.ac.uk",
-            "interval": 10,
-            "expect": {"status": [200]},
-        },
-        {
-            "name": "rabbitmq",
-            "url": "https://rabbitmq.sail-teleport.dk.serp.ac.uk",
-            "interval": 10,
-            "expect": {"status": [200]},
-        },
-    ]
 
     logging.info("listening for events")
     tasks = dict()
@@ -78,41 +68,48 @@ async def watch_events(*args, **kwargs):
     try:
         # Created monitors for current objects
         # TODO Query kubes for existing CanaryHTTPMonitor objects that are visible and iterate over them
-        for monitor in monitors:
-            # Get monitor (simulated)
-            name = monitor["name"]
-            url = monitor["url"]
-            statuses = monitor["expect"]["status"]
-            interval = monitor["interval"] + random.randint(0, 10)
-            logging.info(f"spawning monitor [{name=}]")
-            tasks[name] = asyncio.create_task(
-                monitor_url(name, url, interval, statuses)
-            )
-
-        # Consume events
-        # TODO Subscript to kubes event queue for changes to CanaryHTTPMonitor objects that are visible
-        while True:
-            # Get event (simulated)
-            await asyncio.sleep(30)
-            monitor = random.choice(monitors)
-            name = monitor["name"]
-            url = monitor["url"]
-            statuses = monitor["expect"]["status"]
-            interval = monitor["interval"] + random.randint(0, 10)
-            event = "UPDATED"
-
-            # Cancel the task if it already exists
-            if name in tasks:
-                logging.info(f"cancelling monitor [{name=}]")
-                tasks[name].cancel()
-                await tasks[name]
-
-            # Create a new task at the desired interval
-            if event in ["ADDED", "UPDATED"]:
-                logging.info(f"spawning monitor [{name=}]")
+        # use the context manager to close http sessions automatically
+        async with ApiClient() as api:
+            crds = client.CustomObjectsApi(api)
+            rawmonitors = await crds.list_cluster_custom_object(group="canary.ukserp.ac.uk", version="v1", plural="canaryhttpmonitors")
+            rawmonitors = rawmonitors["items"]
+            for monitor in rawmonitors:
+                name = monitor["metadata"]["name"]
+                url = monitor["spec"]["url"]
+                interval = monitor["spec"]["interval"]
+                if type(monitor["spec"]["status"]) is not list:
+                    statuses = []
+                    statuses.append(monitor["spec"]["status"])
+                else:
+                    statuses = monitor["spec"]["status"]
                 tasks[name] = asyncio.create_task(
                     monitor_url(name, url, interval, statuses)
                 )
+
+        # Consume events
+        # TODO Subscript to kubes event queue for changes to CanaryHTTPMonitor objects that are visible
+        # while True:
+        #     # Get event (simulated)
+        #     await asyncio.sleep(30)
+        #     monitor = random.choice(monitors)
+        #     name = monitor["name"]
+        #     url = monitor["url"]
+        #     statuses = monitor["expect"]["status"]
+        #     interval = monitor["interval"] + random.randint(0, 10)
+        #     event = "UPDATED"
+        #
+        #     # Cancel the task if it already exists
+        #     if name in tasks:
+        #         logging.info(f"cancelling monitor [{name=}]")
+        #         tasks[name].cancel()
+        #         await tasks[name]
+        #
+        #     # Create a new task at the desired interval
+        #     if event in ["ADDED", "UPDATED"]:
+        #         logging.info(f"spawning monitor [{name=}]")
+        #         tasks[name] = asyncio.create_task(
+        #             monitor_url(name, url, interval, statuses)
+        #         )
 
     except asyncio.CancelledError:
         logging.info("cancelled watcher")
