e1dee811c1a80eb85333b518f27f76805c3e9506
joss <joss@chi.swan.ac.uk>
2023-12-01 23:19:23 +0000

    refactor(controller): logging

--- src/canary.py
+++ src/canary.py
@@ -40,7 +40,7 @@ async def monitor_url(name, spec):
                     async with session.get(spec["url"]) as response:
                         status = response.status
 
-                # Check if the status code was acceptible
+                # Check if the status code was acceptable
                 healthy = status == spec["status"]
 
                 logging.info(f"{header} | poll [{status=}] [{healthy=}]")
@@ -69,7 +69,7 @@ async def controller(*args, **kwargs):
     logging.debug(f"controller | {args=}")
     logging.debug(f"controller | {kwargs=}")
 
-    update_interval = kwargs["update-interval"]
+    update_interval = kwargs["update_interval"]
 
     logging.info("controller | loading kube api config")
     k8s.config.load_incluster_config()
@@ -79,7 +79,7 @@ async def controller(*args, **kwargs):
     try:
         while True:
 
-            logging.info("controller | querying kube api for monitors")
+            logging.info("query kube api for monitors")
             async with k8s.client.CustomObjectsApi() as api:
 
                 manifests = await api.list_cluster_custom_object(
@@ -88,55 +88,55 @@ async def controller(*args, **kwargs):
                     plural="canaryhttpmonitors"
                 )
 
-                # Convert the manifests into a dict keyed on namespace.name
-                manifests = {
-                    f"{manifest['metadata']['namespace']}."
-                    f"{manifest['metadata']['name']}": manifest
-                    for manifest in manifests["items"]
-                }
+            # Convert the manifests into a dict keyed on namespace.name
+            manifests = {
+                f"{manifest['metadata']['namespace']}."
+                f"{manifest['metadata']['name']}": manifest
+                for manifest in manifests["items"]
+            }
+            logging.debug(f"discovered {len(manifests)} manifests")
+            logging.debug(f"running {len(monitors)} monitors")
 
-                logging.debug(f"controller | [manifests={len(manifests)}]")
+            # Cancel existing monitors that are not found in the live manifests
+            for name, monitor in monitors.items():
 
-                # Cancel existing monitors that are not found in the live manifests
-                for name, monitor in monitors.items():
+                if name not in manifests:
+                    logging.info(f"canceling monitor [{name=}]")
+                    monitors[name]["task"].cancel()
+                    # Don't care about waiting for this
+                    # await monitors[name]["task"]
+                    monitors.pop(name)
 
-                    if name not in manifests:
-                        logging.info(f"controller | canceling monitor [{name=}]")
-                        monitors[name]["task"].cancel()
-                        # Don't care about waiting for this
-                        # await monitors[name]["task"]
-                        monitors.pop(name)
+            # Create or re-create monitors to match the live manifests
+            for name, manifest in manifests.items():
 
-                # Create or re-create monitors to match the live manifests
-                for name, manifest in manifests.items():
+                if (name in monitors) and (monitors[name]["spec"] != manifest["spec"]):
+                    logging.info(f"recreating monitor [{name=}]")
+                    monitors[name]["task"].cancel()
+                    await monitors[name]["task"]
+                    monitors.pop(name)
 
-                    if (name in monitors) and (monitors[name]["spec"] != manifest["spec"]):
-                        logging.info(f"controller | recreating monitor [{name=}]")
-                        monitors[name]["task"].cancel()
-                        await monitors[name]["task"]
-                        monitors.pop(name)
-
-                    if name not in monitors:
-                        logging.info(f"controller | spawning monitor [{name=}]")
-                        monitors[name] = dict(
-                            name=name,
-                            spec=manifest["spec"],
-                            task=asyncio.create_task(
-                                monitor_url(name=name, spec=manifest["spec"])
-                            )
+                if name not in monitors:
+                    logging.info(f"spawning monitor [{name=}]")
+                    monitors[name] = dict(
+                        name=name,
+                        spec=manifest["spec"],
+                        task=asyncio.create_task(
+                            monitor_url(name=name, spec=manifest["spec"])
                         )
+                    )
 
             # Pause before polling the kube api again
             await asyncio.sleep(update_interval)
 
     except asyncio.CancelledError:
-        logging.info("controller | cancelled")
+        logging.info("cancelled")
 
     finally:
-        logging.info("controller | halting")
+        logging.info("halting")
 
         for name, monitor in monitors.items():
-            logging.info(f"controller | canceling monitor [{name=}]")
+            logging.info(f"canceling monitor [{name=}]")
             monitor["task"].cancel()
 
         await asyncio.gather(*map(lambda m: m["task"], monitors.values()))
@@ -152,9 +152,9 @@ async def controller(*args, **kwargs):
     show_default=True
 )
 def main(*args, **kwargs):
-    logging.info("canary | spawning controller")
+    logging.info("spawning controller")
     asyncio.run(controller(*args, **kwargs))
-    logging.info("canary | halting")
+    logging.info("halting")
 
 
 if __name__ == "__main__":
