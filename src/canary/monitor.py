38d809f6f4656b7fa6a7c47be6b01e905f2c3e7f
joss <joss@chi.swan.ac.uk>
2023-12-06 12:40:24 +0000

    feat: adjust poll timeouts

--- src/canary/monitor.py
+++ src/canary/monitor.py
@@ -8,34 +8,65 @@ from .metrics import EventWindowGauge
 
 MIN_MONITOR_INTERVAL = 5  # seconds
 
-LABELS = ["k8s_node_name", "k8s_pod_name", "k8s_pod_namespace", "monitor", "url"]
+LABELS = [
+    "k8s_node_name",
+    "k8s_pod_name",
+    "k8s_pod_namespace",
+    "monitor",
+    "url"
+]
 
-STATUS_LASTSEEN_GAUGE = Gauge(
-    name="canary_status_lastseen",
-    documentation="Timestamp of the most recent time a status code was observed.",
-    labelnames=LABELS+["status"],
-)
-UNHEALTHY_LASTSEEN_GAUGE = Gauge(
-    name="canary_unhealthy_lastseen",
-    documentation="Timestamp of the most recent time a monitor was unhealthy.",
+# STATUS_LASTSEEN_GAUGE = Gauge(
+#     name="canary_status_lastseen",
+#     documentation="Timestamp of the most recent time a status code was observed.",
+#     labelnames=LABELS+["status"],
+# )
+# UNHEALTHY_LASTSEEN_GAUGE = Gauge(
+#     name="canary_unhealthy_lastseen",
+#     documentation="Timestamp of the most recent time a monitor was unhealthy.",
+#     labelnames=LABELS,
+# )
+# UNHEALTHY_EVENT_GAUGES = [
+#     EventWindowGauge(
+#         name="canary_unhealthy_1m",
+#         documentation="Number of times the monitor was unhealthy in the last minute.",
+#         labelnames=LABELS,
+#         window=(60 * 1)
+#     ),
+#     EventWindowGauge(
+#         name="canary_unhealthy_5m",
+#         documentation="Number of times the monitor was unhealthy in the last 5 minutes.",
+#         labelnames=LABELS,
+#         window=(60 * 5)
+#     ),
+#     EventWindowGauge(
+#         name="canary_unhealthy_1h",
+#         documentation="Number of times the monitor was unhealthy in the last hour.",
+#         labelnames=LABELS,
+#         window=(60 * 60)
+#     )
+# ]
+HEALTHY_LASTSEEN_GAUGE = Gauge(
+    name="canary_healthy_lastseen",
+    documentation="Timestamp of the most recent time a monitor was healthy.",
     labelnames=LABELS,
 )
-UNHEALTHY_EVENT_GAUGES = [
+HEALTHY_EVENT_GAUGES = [
     EventWindowGauge(
-        name="canary_unhealthy_1m",
-        documentation="Number of times the monitor was unhealthy in the last minute.",
+        name="canary_healthy_1m",
+        documentation="Number of times the monitor was healthy in the last minute.",
         labelnames=LABELS,
         window=(60 * 1)
     ),
     EventWindowGauge(
-        name="canary_unhealthy_5m",
-        documentation="Number of times the monitor was unhealthy in the last 5 minutes.",
+        name="canary_healthy_5m",
+        documentation="Number of times the monitor was healthy in the last 5 minutes.",
         labelnames=LABELS,
         window=(60 * 5)
     ),
     EventWindowGauge(
-        name="canary_unhealthy_1h",
-        documentation="Number of times the monitor was unhealthy in the last hour.",
+        name="canary_healthy_1h",
+        documentation="Number of times the monitor was healthy in the last hour.",
         labelnames=LABELS,
         window=(60 * 60)
     )
@@ -48,66 +79,58 @@ async def Monitor(name: str, spec: dict, labels: dict):
     This co-routine loops forever unless it is externally cancelled, such as to recreate it with new settings..
     """
 
+    # Hard clamp the interval to minimum of 5s to prevent DOS runaway
+    # Still could be bad if running on a lot of nodes
+    interval = max(MIN_MONITOR_INTERVAL, int(spec["interval"]))
+
+    # Allow this to throw an exception if the url is invalid
+    url = spec["url"]
+    urllib.parse.urlparse(url)
+
+    # TODO handle this as a list of valid statuses
+    expected_status = str(spec["status"])
+
+    # Add extra labels
+    labels |= dict(monitor=name, url=url)
+
+    header = f"[{name=}] [{interval=}] [{url=}]"
+    logging.info(f"{header} | polling")
+
     try:
         while True:
+            # Spawn a task to track the minimum amount of time to the next iteration and return immediately
+            interval_task = asyncio.create_task(asyncio.sleep(interval))
+
+            healthy = False
             try:
-                # Hard clamp the interval to minimum of 5s to prevent DOS runaway
-                # Still could be bad if running on a lot of nodes
-                interval = max(MIN_MONITOR_INTERVAL, int(spec["interval"]))
+                # Poll the url
+                timeout = aiohttp.ClientTimeout(total=interval)
+                async with aiohttp.ClientSession(timeout=timeout) as session:
+                    async with session.get(url, timeout=interval) as response:
+                        status = str(response.status)
 
-                # Allow this to throw an exception if the url is invalid
-                url = spec["url"]
-                urllib.parse.urlparse(url)
+                # Check if the status code was acceptable
+                healthy = status == expected_status
+                logging.info(f"{header} | poll [{status=}] [{healthy=}]")
 
-                # TODO handle this as a list of valid statuses
-                expected_status = str(spec["status"])
-
-                # Add extra labels
-                labels |= dict(monitor=name, url=url)
-
-                header = f"[{name=}] [{interval=}] [{url=}]"
-                logging.info(f"{header} | polling")
-
-                while True:
-                    # Spawn a task to track the minimum amount of time to the next iteration and return immediately
-                    interval_task = asyncio.create_task(asyncio.sleep(interval))
-
-                    try:
-                        # Poll the url
-                        async with aiohttp.ClientSession() as session:
-                            async with session.get(url) as response:
-                                status = str(response.status)
-
-                        # Check if the status code was acceptable
-                        healthy = status == expected_status
-
-                        # Update the unhealthy metric
-                        if not healthy:
-                            UNHEALTHY_LASTSEEN_GAUGE.labels(**labels).set_to_current_time()
-                            for gauge in UNHEALTHY_EVENT_GAUGES:
-                                gauge.update(labels=labels)
-
-                        logging.info(f"{header} | poll [{status=}] [{healthy=}]")
-
-                        # Update the status metric
-                        STATUS_LASTSEEN_GAUGE.labels(**(labels | dict(status=status))).set_to_current_time()
-
-                    except Exception as ex:
-                        logging.exception(f"{header} | ERROR", exc_info=ex)
-
-                        # Update the unhealthy metric
-                        UNHEALTHY_LASTSEEN_GAUGE.labels(**labels).set_to_current_time()
-                        for gauge in UNHEALTHY_EVENT_GAUGES:
-                            gauge.update(labels=labels)
-
-                    # Await the minimum interval, returns immediately if it's already passed
-                    await interval_task
-
-            except asyncio.CancelledError as ex:
-                raise ex
             except Exception as ex:
-                logging.exception(f"{header} | ERROR", exc_info=ex)
-                await asyncio.sleep(5)
+                logging.exception(f"{header} | poll error", exc_info=ex)
+
+            # Update the unhealthy metric
+            if healthy:
+                HEALTHY_LASTSEEN_GAUGE.labels(**labels).set_to_current_time()
+                for gauge in HEALTHY_EVENT_GAUGES:
+                    gauge.update(labels=labels)
+            # else:
+            #     UNHEALTHY_LASTSEEN_GAUGE.labels(**labels).set_to_current_time()
+            #     for gauge in UNHEALTHY_EVENT_GAUGES:
+            #         gauge.update(labels=labels)
+
+            # Update the status metric
+            # STATUS_LASTSEEN_GAUGE.labels(**(labels | dict(status=status))).set_to_current_time()
+
+            # Await the minimum interval, returns immediately if it's already passed
+            await interval_task
 
     except asyncio.CancelledError:
         logging.info(f"{header} | cancelled")
